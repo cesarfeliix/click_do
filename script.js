b12d4c09761a2027377e1aeb515d19e9a4b01b8b
cesarfeliix <feliixcesar@gmail.com>
2023-01-18 23:13:46 -0300

    Adicionados localStorage e manifest.json

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
-const inputContainer = document.getElementById("input-container");
+
+// Load existing tasks from local storage
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+for (let i = 0; i < tasks.length; i++) {
+    const newTask = document.createElement("li");
+    newTask.innerHTML = tasks[i];
+    taskList.appendChild(newTask);
+}
 
 taskInput.addEventListener("keyup", function(e) {
     if (e.keyCode === 13) {
@@ -16,12 +23,16 @@ function addTask(){
     newTask.innerHTML = taskInput.value;
     taskList.appendChild(newTask);
     taskInput.value = "";
+    tasks.push(newTask.innerHTML);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 taskList.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
         if(e.target.classList.contains('completed')){
             e.target.remove();
+            tasks.splice(tasks.indexOf(e.target.innerHTML), 1);
+            localStorage.setItem("tasks", JSON.stringify(tasks));
         }else{
             e.target.classList.add('completed');
         }
